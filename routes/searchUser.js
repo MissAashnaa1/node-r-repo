468f611036aa38b8192fe46d0d92e61c8ac243b2
MissAashnaa1 <97970004+MissAashnaa1@users.noreply.github.com>
2023-06-15 14:32:10 +0530

    Update searchUser.js require('path')

--- routes/searchUser.js
+++ routes/searchUser.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const router=express.Router()
 const app=express()
+const path = require('path')
 const fs = require("fs");
 
 const dir="H:\\javascript\\node-project";
@@ -9,7 +10,7 @@ router
     
     // res.render("searchUser.ejs", { noUser: false, data:[{key1:"kay1",key2:"key2",key3:"key3"}, {key1:"kay1",key2:"key2",key3:"key3"},{key1:"kay1",key2:"key2",key3:"key3"}]})
     let info=[];
-    fs.readFile(__dirname + "/data.json", "utf-8", function (err, data){
+    fs.readFile(path.join(__dirname,'..','data.json'), "utf-8", function (err, data){
         // console.log(data,typeof data)
         info = JSON.parse(data);
         // console.log(data,typeof data)
@@ -30,7 +31,7 @@ router
             console.log("if")
             let info=[]
             // let noUser = false;
-            fs.readFile(__dirname + "/data.json", "utf-8", (err, data)=>{
+            fs.readFile(path.join(__dirname,'..','data.json'), "utf-8", (err, data)=>{
                 if(err){
                     console.log(err)
                 }else{
@@ -48,7 +49,7 @@ router
         else if(mobile=="" && name!=""){
             console.log("elseif 1")
             let info=[];
-            fs.readFile(_dirname + "/data.json", "utf-8", function (err, data){
+            fs.readFile(path.join(__dirname,'..','data.json'), "utf-8", function (err, data){
                 console.log(data,typeof data)
                 data = JSON.parse(data);
                 console.log(data,typeof data)
@@ -95,7 +96,7 @@ router
     })
 .delete('/',(req,res)=>{
     // console.log(req.body)
-    fs.readFile(__dirname +'/data.json','utf-8',(err,data)=>{
+    fs.readFile(path.join(__dirname,'..','data.json'),'utf-8',(err,data)=>{
         if(data.length == 0) info =[]
         else info = JSON.parse(data);
 
@@ -107,7 +108,7 @@ router
             }
         }
 
-        fs.writeFile(__dirname+"/data.json",JSON.stringify(info),(err)=>{
+        fs.writeFile(path.join(__dirname,'..','data.json'),JSON.stringify(info),(err)=>{
             if(err) res.json({msg:"error in writing data.json",success:false})
             else res.json({msg:'info delete req',success:true})
             return;
@@ -118,7 +119,7 @@ router
     // console.log(req.body)
     // res.redirect('/createUser')
 
-    fs.readFile(__dirname +'/data.json','utf-8',(err,data)=>{
+    fs.readFile(path.join(__dirname,'..','data.json'),'utf-8',(err,data)=>{
         if(data.length == 0) info =[]
         else info = JSON.parse(data);
         for(let i=0; i < info.length;i++){
